9eb331132c9b58a8d8ff79d52452aa2e887b3e3e
moesnow <i@kotori.top>
2022-09-23 12:30:40 +0800

    first commit

---
+++ client.c
@@ -0,0 +1,74 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define LOG(fmt, args...) \
+    printf("[%s %d]" fmt, __FILE__, __LINE__, ##args)
+
+void errExit(const char *desc) {
+    perror(desc);
+    exit(-1);
+}
+
+void *recvMsg(void *arg) {
+    long fd = (long)arg;
+    for (;;) {
+        char msg[1024] = {};
+        if (recv(fd, msg, sizeof(msg), 0) <= 0) {
+            return NULL;
+        }
+        printf("%s\n>", msg);
+        fflush(stdout);
+    }
+}
+void connectServer(const char *ip, unsigned short port) {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        errExit("socket");
+    }
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(ip);
+    int ret = connect(sockfd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (ret != 0) {
+        errExit("connect");
+    }
+    char name[128] = {};
+    printf("请给自己取个网名：");
+    scanf("%s", name);
+    printf(">");
+    send(sockfd, name, strlen(name) + 1, 0);
+
+    pthread_t id;
+    pthread_create(&id, NULL, recvMsg, (void *)(long)sockfd);
+    for (;;) {
+        char buf[1024] = {};
+        scanf("%s", buf);
+        printf(">");
+        ret = send(sockfd, buf, strlen(buf) + 1, 0);
+        if (ret <= 0) {
+            LOG("%s\n", strerror(errno));
+            break;
+        }
+        if (strcmp(buf, "!quit") == 0) {
+            break;
+        }
+    }
+    close(sockfd);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("用法：%s serverip serverport\n", argv[0]);
+        return -1;
+    }
+    connectServer(argv[1], atoi(argv[2]));
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,132 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define NAME_LEN 128
+#define MAX_CLIENTS 256
+
+struct Client {
+    int fd;
+    char name[NAME_LEN];
+};
+
+struct Client clts[MAX_CLIENTS];
+size_t size = 0;
+
+void errExit(const char *desc) {
+    perror(desc);
+    exit(-1);
+}
+
+void broadcast(const char *msg, int fd) {
+    int i;
+    for (i = 0; i < size; i++) {
+        if (clts[i].fd != fd) {
+            int ret = send(clts[i].fd, msg, strlen(msg) + 1, 0);
+            if (ret <= 0) {
+            }
+        }
+    }
+}
+
+void run(int listenFd) {
+    fd_set fdsets;
+    int i, ret;
+    int maxfd;
+    for (;;) {
+        maxfd = listenFd;
+        FD_ZERO(&fdsets);
+        FD_SET(listenFd, &fdsets);
+        for (i = 0; i < size; i++) {
+            FD_SET(clts[i].fd, &fdsets);
+            if (clts[i].fd > maxfd) {
+                maxfd = clts[i].fd;
+            }
+        }
+        ret = select(maxfd + 1, &fdsets, NULL, NULL, NULL);
+        while (ret > 0) {
+            if (FD_ISSET(listenFd, &fdsets)) {
+                struct sockaddr_in caddr;
+                socklen_t addrlen = sizeof(caddr);
+                int cfd = accept(listenFd, (struct sockaddr *)&caddr, &addrlen);
+                printf("%s %hu\n", inet_ntoa(caddr.sin_addr), ntohs(caddr.sin_port));
+                clts[size].fd = cfd;
+                strcpy(clts[size].name, "");
+                ret--;
+                size++;
+            }
+            for (i = 0; ret > 0 && i < size; i++) {
+                int n = 1;
+                if (FD_ISSET(clts[i].fd, &fdsets)) {
+                    if (strcmp(clts[i].name, "") == 0) {
+                        recv(clts[i].fd, clts[i].name, NAME_LEN, 0);
+                        char msg[1024] = {};
+                        char buffer[10];
+                        sprintf(buffer, "%ld", size);
+                        strcpy(msg, "当前在线人数:");
+                        strcat(msg, buffer);
+                        send(clts[i].fd, msg, strlen(msg) + 1, 0);
+                        strcpy(msg, clts[i].name);
+                        strcat(msg, "进入聊天室，大家欢迎!");
+                        broadcast(msg, clts[i].fd);
+                    } else {
+                        char msg[1024] = {};
+                        strcpy(msg, clts[i].name);
+                        strcat(msg, ":");
+                        int len = strlen(msg);
+                        n = recv(clts[i].fd, msg + len, 1024 - len, 0);
+                        if (n <= 0) {
+                            strcpy(msg, clts[i].name);
+                            strcat(msg, "离开了聊天室!");
+                        }
+                        broadcast(msg, clts[i].fd);
+                    }
+                    ret--;
+                }
+                if (n <= 0) {
+                    close(clts[i].fd);
+                    clts[i] = clts[size - 1];
+                    size--;
+                    i--;
+                }
+            }
+        }
+    }
+}
+
+void server(const char *ip, unsigned short port) {
+    int sfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sfd == -1) {
+        errExit("socket");
+    }
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(ip);
+    int ret;
+    ret = bind(sfd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (ret != 0) {
+        errExit("bind");
+    }
+    ret = listen(sfd, 5);
+    if (ret != 0) {
+        errExit("listen");
+    }
+    run(sfd);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("usage: %s <ip> <port>\n", argv[0]);
+        return -1;
+    }
+
+    server(argv[1], atoi(argv[2]));
+
+    return 0;
+}
